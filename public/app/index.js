814d7682ffd01108773e44623f2dc140417ca8df
TRomesh <tharakaromesh@gmail.com>
2016-11-03 23:21:34 +0530

    adding 1st react file

---
+++ public/app/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component{
+
+      render(){
+        return(
+             <h1>Hello Sinhala Parser</h1>
+        );
+      }
+
+}
+
+render(<App/>,document.getElementById('app'));
