205eddd03919d0c92b90be76184a1ee9de3597e9
TRomesh <tharakaromesh@gmail.com>
2016-11-04 00:34:50 +0530

    adding cytoscape-dagre graph

--- public/src/app/components/parser/ParserTagBox.react.js
+++ public/src/app/components/parser/ParserTagBox.react.js
@@ -86,6 +86,74 @@ class ParserTagBox extends React.Component{
 
 
 
+   renderCytoscapeElement(){
+
+        console.log('* Cytoscape.js is rendering the graph..');
+
+        this.cy = cytoscape(
+        {
+            container: document.getElementById('cy'),
+
+            boxSelectionEnabled: false,
+            autounselectify: true,
+
+            style: cytoscape.stylesheet()
+                .selector('node')
+                .css({
+                    'height': 80,
+                    'width': 80,
+                    'background-fit': 'cover',
+                    'border-color': pinkA400,
+                    'border-width': 3,
+                    'border-opacity': 0.5,
+                    'content': 'data(id)',
+                    'text-valign': 'center',
+                })
+                .selector('edge')
+                .css({
+                    'width': 6,
+                    'target-arrow-shape': 'triangle',
+                    'line-color': blueA400,
+                    'target-arrow-color': blueA400,
+                    'curve-style': 'bezier'
+                })
+                ,
+            elements: {
+                nodes: [
+
+                    { data: { id: 'bird' } },
+                    { data: { id: 'ladybug' } },
+                    { data: { id: 'aphid' } },
+                    { data: { id: 'rose' } },
+                    { data: { id: 'grasshopper' } },
+                    { data: { id: 'plant' } },
+                    { data: { id: 'wheat' } }
+                ],
+                edges: [
+
+                    { data: { source: 'bird', target: 'ladybug' } },
+                    { data: { source: 'bird', target: 'grasshopper' } },
+                    { data: { source: 'grasshopper', target: 'plant' } },
+                    { data: { source: 'grasshopper', target: 'wheat' } },
+                    { data: { source: 'ladybug', target: 'aphid' } },
+                    { data: { source: 'aphid', target: 'rose' } }
+                ]
+            },
+
+            layout: {
+                name: 'breadthfirst',
+                directed: true,
+                padding: 10
+            }
+            });
+    }
+
+    componentDidMount(){
+     this.renderCytoscapeElement();
+    }
+
+
+
       render(){
         return(
           <div>
