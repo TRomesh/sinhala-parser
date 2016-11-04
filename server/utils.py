3a300b22a2a98625a417b79cffeebecb3e178cf5
Isuru Dilhan <isuru.dilhan@yahoo.com>
2016-11-04 12:26:45 +0530

    Add Parser to utils

---
+++ server/utils.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+from server import Tagger, Tree
+
+
+class Parser:
+
+    def treebuilder(self, data):
+        words = data.split()
+
+        tagger = Tagger()
+        label = tagger.tag(words)
+
+        tree = Tree()
+        result = tree.depict_tree(words, label)
+        result.draw()
+
