6e236cb0bfef6b824c5d568d110b06723f69b732
Singhabahu, Chamara <mails.chamara@gmail.com>
2016-11-11 09:44:26 +0530

    Reduce redundancy in code

--- server/utils.py
+++ server/utils.py
@@ -4,27 +4,24 @@ from tree import Tree
 
 
 class Parser:
-
     @staticmethod
-    def tree_builder_json(data):
+    def tree_builder(data):
         words = data.split()
 
         tagger = Tagger()
         label = tagger.tag(words)
 
         tree = Tree()
-        result = tree.depict_tree(words, label)
+        return tree, tree.depict_tree(words, label)
+
+    @staticmethod
+    def tree_builder_json(data):
+        tree, result = Parser.tree_builder(data)
         result_json = tree.get_json_result(result)
         return result_json
 
     @staticmethod
     def tree_builder_xml(data):
-        words = data.split()
-
-        tagger = Tagger()
-        label = tagger.tag(words)
-
-        tree = Tree()
-        result = tree.depict_tree(words, label)
+        tree, result = Parser.tree_builder(data)
         result_xml = tree.get_xml_result(result)
         return result_xml
