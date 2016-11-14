a954dc374870496c2f0cb6ae3454adf46356fa8d
TRomesh <tharakaromesh@gmail.com>
2016-11-14 13:47:45 +0530

    add Actions content

---
+++ public/src/app/Actions/Actions.js
@@ -0,0 +1,24 @@
+import AppConstants from '../Constants/AppConstants';
+import {dispatch,register} from '../Dispatcher/Dispatcher';
+import axios from 'axios';
+
+export default {
+   SendDataToTag(sentence){
+     axios.get('http://35.163.71.75/v1/api/'+sentence).then((response)=>{
+        dispatch({ actionType: AppConstants.ADD_DATA,data:response.data});
+       }).catch((err)=>{
+          console.log(err);
+      });
+
+   },
+   RemoveTaggedData(item){
+     axios.get().then((response)=>{
+        dispatch({ actionType: AppConstants.REMOVE_DATA,data:response});
+       }).catch((err)=>{
+          console.log(err);
+      });
+   },
+   ClearTextFieldErorrMsg(item){
+      dispatch({ actionType: AppConstants.CLEAR_DATA,data:''});
+   }
+}
