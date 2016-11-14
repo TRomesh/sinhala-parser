b0e9dfac524f3cda2d7db71c26c01668b32fc544
Saad Sahibjan <saad.sahibjan@gmail.com>
2016-11-14 16:11:38 +0530

    Make method calls within the class with self prefix

--- server/utils.py
+++ server/utils.py
@@ -29,11 +29,11 @@ class Parser:
         return tree, tree.depict_tree(words, label)
 
     def tree_builder_json(self, data):
-        tree, result = Parser.tree_builder(data)
+        tree, result = self.tree_builder(data)
         result_json = tree.get_json_result(result)
         return result_json
 
     def tree_builder_xml(self, data):
-        tree, result = Parser.tree_builder(data)
+        tree, result = self.tree_builder(data)
         result_xml = tree.get_xml_result(result)
         return result_xml
